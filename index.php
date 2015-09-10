8a96f1ee34369b9e4df1a1b745dbfaf636974d73
Diego Delfin <diegodelfinacharte@gmail.com>
2015-09-10 03:02:53 -0500

    agregando font-awesome,avanzando maquetado

--- index.php
+++ index.php
@@ -4,14 +4,15 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>app cat</title>
-	<link rel="stylesheet"  href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
-	<link rel="stylesheet"  href="public/css/app.css" />
+	<link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="bower_components/components-font-awesome/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="public/css/app.css" />
 </head>
 <body>
 	<div id="landing-video" class=" text-center">
-		<h2 >TECNOLOGÍA</h2>
-		<p>LA INNOVACIÓN ESTA EN NUESTRA SANGRE</p>
-		<a data-toggle="modal"  data-target="#ModalYTCat"><img src="public/img/boton-play.png" height="112" width="112"></a>
+		<div class="title-landing-video">TECNOLOGÍA</div>
+		<div class="description-landing-video">LA INNOVACIÓN ESTA EN NUESTRA SANGRE</div>
+		<a data-toggle="modal" class="btn-landing-playvideo" data-target="#ModalYTCat"><img src="public/img/boton-play.png" height="112" width="112"></a>
 	</div>
 	<div id="landing-blockthumb">
 		<div class="container text-center inner-blockthumb">
@@ -32,38 +33,68 @@
 			<div class="col-md-3 col-lg-3">
 				<div class="block-thumb">
 					<div class="title-block-thumb">ERGO</div>
-					<p class="description-block-thumb">DISEÑO ERGONÓMICO CON MAYOR FLEXIBILIDAD</p>
+					<p class="description-block-thumb">DISEÑO ERGONÓMICO <br /> CON MAYOR FLEXIBILIDAD</p>
 					<img src="public/img/ergo-thumb.png" class="block-img-thumb" />
 				</div>
 			</div>
 			<div class="col-md-3 col-lg-3">
 				<div class="block-thumb">
 					<div class="title-block-thumb">ERGO LITE</div>
-					<p class="description-block-thumb">DISEÑO ERGONÓMICO DE ERGO MUCHO MÁS LIGERO</p>
+					<p class="description-block-thumb">DISEÑO ERGONÓMICO <br /> DE ERGO MUCHO MÁS LIGERO</p>
 					<img src="public/img/ergolite-thumb.png" class="block-img-thumb" />
 				</div>
 			</div>
 		</div>
 	</div>
-	<div class="container">
-		<h2 class="text-center">CONTÁCTANOS</h2>
-		<form>
-			<div class="form-group">
-				<input type="text" placeholder="NOMBRE" class="form-control" />
+	<div class="srx">
+		<div class="srx-title">SRX</div>
+	</div>
+	<div class="srx-more">
+		<div class="container text-center">
+			<div class="col-md-4">
+				<div class="block-srx-more">
+					<img src="public/img/srx-1.png"  width="213" height="163" alt="" />
+					<div class="src-more-title">DYNAMO 6'' CT</div>
+				</div>
 			</div>
-			<div class="form-group">
-				<input type="text" placeholder="MAIL" class="form-control" />
+			<div class="col-md-4">
+				<div class="block-srx-more">
+					<img src="public/img/srx-2.png"  width="213" height="163" alt="" />
+					<div class="src-more-title">DAWSON 6'' ct</div>
+				</div>
 			</div>
-			<div class="form-group">
-				<textarea rows="9" cols="50" placeholder="DESCRIPCIÓN"  class="form-control" ></textarea>
+			<div class="col-md-4">
+				<div class="block-srx-more">
+					<img src="public/img/srx-3.png"  width="213" height="163" alt="" />
+					<div class="src-more-title">SENSOR HI ST</div>
+				</div>
 			</div>
-			<div class="form-group">
-				<label class="col-md-4 control-label" for="singlebutton"></label>
-   				<div class="col-md-4 text-center">
-        					<button type="submit" class="btn btn-default">Enviar</button>
-    				</div>
-			</div>
-		</form>
+		</div>
+		<div class="containerbtn text-center">
+			<a href="#" class="btnhere-src-more">DESCUBRE MÁS AQUÍ</a>
+		</div>
+	</div>
+	<div class="form-contact">
+		<div class="container">
+			<h2 class="text-center">CONTÁCTANOS</h2>
+			<form>
+				<div class="form-group">
+					<input type="text" placeholder="NOMBRE" class="form-control" />
+				</div>
+				<div class="form-group">
+					<input type="text" placeholder="MAIL" class="form-control" />
+				</div>
+				<div class="form-group">
+					<textarea rows="9" cols="50" placeholder="DESCRIPCIÓN"  class="form-control" ></textarea>
+				</div>
+				<div class="form-group">
+					<label class="col-md-4 control-label" for="singlebutton"></label>
+	   				<div class="col-md-4 text-center">
+	        			<button type="submit" class="btnsubmit-form">Enviar</button>
+	    			</div>
+				</div>
+			</form>
+		</div>
 	</div>
 	<script src="bower_components/jquery/dist/jquery.min.js"></script>
 	<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
